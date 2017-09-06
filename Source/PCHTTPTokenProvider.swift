7907031688069db5b5f2529d6127d43fd25f3939
Luka Bratos <luka@pusher.com>
2017-09-06 16:55:53 +0100

    Remove comments

--- Source/PCHTTPTokenProvider.swift
+++ Source/PCHTTPTokenProvider.swift
@@ -1,10 +1,3 @@
-//
-//  PCHTTPTokenProvider.swift
-//  PusherChatkit
-//
-//  Created by Luka on 06/09/2017.
-//
-
 import PusherPlatform
 
 public typealias PCHTTPTokenProvider = PPHTTPEndpointTokenProvider
