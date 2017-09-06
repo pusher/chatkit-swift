a76155f5be93611e88353ab78d1600e4a276bcfe
Luka Bratos <luka@pusher.com>
2017-09-06 15:50:02 +0100

    Add PCHTTPTokenProvider

---
+++ Source/PCHTTPTokenProvider.swift
@@ -0,0 +1,10 @@
+//
+//  PCHTTPTokenProvider.swift
+//  PusherChatkit
+//
+//  Created by Luka on 06/09/2017.
+//
+
+import PusherPlatform
+
+typealias PCHTTPTokenProvider = PPHTTPEndpointTokenProvider
