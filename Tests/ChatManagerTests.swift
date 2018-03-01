9c7832dc8cbefc89b86d8d7e82a13274907e5d80
Hamilton Chapman <hamchapman@gmail.com>
2018-03-01 11:42:24 +0000

    Get tests working as expected and bump PusherPlatform to 0.4.1

---
+++ Tests/ChatManagerTests.swift
@@ -0,0 +1,29 @@
+import XCTest
+import PusherPlatform
+@testable import PusherChatkit
+
+class ChatManagerTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+    }
+
+    func testInstancesHaveSDKInfoHeadersSet() {
+        let chatManager = ChatManager(
+            instanceLocator: "v1:test:test",
+            tokenProvider: dummyTokenProvider,
+            userId: "testinator"
+        )
+
+        let sdkProductName = "chatkit"
+        let sdkVersion = "0.6.3"
+        let sdkLanguage = "swift"
+
+        let baseClientHeaders = chatManager.instance.client.generalRequestURLSession.configuration.httpAdditionalHeaders as! [String: String]
+
+        XCTAssertEqual(baseClientHeaders["X-SDK-Product"], sdkProductName)
+        XCTAssertEqual(baseClientHeaders["X-SDK-Version"], sdkVersion)
+        XCTAssertEqual(baseClientHeaders["X-SDK-Language"], sdkLanguage)
+    }
+}
+
+let dummyTokenProvider = PCTokenProvider(url: "https://test.testing/test")
