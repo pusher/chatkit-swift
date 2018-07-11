f93a0de60acc2ce4c5f142252b567f57e60cd4d4
Callum Oakley <c.oakley108@gmail.com>
2018-07-11 16:18:58 +0100

    test setting cursor and newCursor hook

--- Tests/CursorTests.swift
+++ Tests/CursorTests.swift
@@ -2,65 +2,99 @@ import XCTest
 import PusherPlatform
 @testable import PusherChatkit
 
-func testUser(userId: String) -> ChatManager {
-    return ChatManager(
-        instanceLocator: testInstanceLocator,
-        tokenProvider: PCTokenProvider(url: testInstanceTokenProviderURL),
-        userId: userId,
-        logger: TestLogger()
-    )
-}
+var alice: PCCurrentUser?
+var bob: PCCurrentUser?
+var roomId: Int?
+var doneSetUp = false
 
 class CursorTests: XCTestCase {
-    var aliceManager: ChatManager?
-    var bobManager: ChatManager?
-    var room: PCRoom?
-    
-    // Currently these tests only pass if the users "alice" and "bob" already exist.
     override func setUp() {
-        let ex = expectation(description: "Room is created")
-        if aliceManager == nil {
-            aliceManager = testUser(userId: "alice")
-        }
-        if bobManager == nil {
-            aliceManager = testUser(userId: "bob")
-        }
-        if room == nil {
-            aliceManager?.connect(delegate: TestingChatManagerDelegate()) { alice, error in
-                XCTAssertNil(error)
-                
-                alice?.createRoom(name: "Alice's room") { r, error in
-                    XCTAssertNil(error)
-                    
-                    self.room = r
-                    
-                    ex.fulfill()
-                }
-            }
-        } else {
-            ex.fulfill()
-        }
         super.setUp()
-        waitForExpectations(timeout: 5)
+
+        if !doneSetUp {
+            alice = user(id: "alice")
+            bob = user(id: "bob")
+            roomId = createRoom(user: user(id: "alice"), roomName: "mushroom", addUserIds: ["bob"]).id
+            doneSetUp = true
+        }
     }
-    
+
     func testOwnReadCursorUndefinedIfNotSet() {
-        let ex = expectation(description: "Get empty cursor after connection")
-        
-        aliceManager?.connect(delegate: TestingChatManagerDelegate()) { alice, error in
+        let cursor = try! user(id: "alice").readCursor(roomId: roomId!)
+        XCTAssertNil(cursor)
+    }
+
+    // TODO own read cursor?
+
+    func testNewReadCursorHook() {
+        class AliceRoomDelegate: NSObject, PCRoomDelegate {
+            let ex: XCTestExpectation
+
+            init(expectation: XCTestExpectation) {
+                ex = expectation
+            }
+
+            func newCursor(cursor: PCCursor) {
+                XCTAssertEqual(cursor.position, 42)
+                ex.fulfill()
+            }
+        }
+
+        let ex = expectation(description: "received new read cursor")
+        let aliceRoomDelegate = AliceRoomDelegate(expectation: ex)
+        alice!.subscribeToRoom(
+            room: alice!.rooms.first(where: { $0.id == roomId! })!,
+            roomDelegate: aliceRoomDelegate
+        )
+
+        // TODO can we wait on the subscription to finish (without sleeping...)?
+        sleep(2)
+
+        bob?.setReadCursor(position: 42, roomId: roomId!) { error in
             XCTAssertNil(error)
-            XCTAssertNotNil(alice)
-            
-            let cursor = try! alice!.readCursor(roomId: self.room!.id)
-            
-            XCTAssertNil(cursor)
+        }
+
+        waitForExpectations(timeout: 50) // why doesn't this work?
+    }
+
+    func user(id: String, delegate: PCChatManagerDelegate = TestingChatManagerDelegate()) -> PCCurrentUser {
+        var user: PCCurrentUser?
+
+        let ex = expectation(description: "connected as user with ID \(id)")
+
+        let chatManager = ChatManager(
+            instanceLocator: testInstanceLocator,
+            tokenProvider: PCTokenProvider(url: testInstanceTokenProviderURL),
+            userId: id,
+            logger: TestLogger()
+        )
+
+        chatManager.connect(delegate: delegate) { u, error in
+            XCTAssertNil(error)
+            XCTAssertNotNil(u)
+
+            user = u
             ex.fulfill()
         }
-        
+
         waitForExpectations(timeout: 5)
+        return user!
     }
-    
-    func testSomethingElse() {
-        print("TESTING SOMETHING ELSE!")
+
+    func createRoom(user: PCCurrentUser, roomName: String, addUserIds: [String] = []) -> PCRoom {
+        var room: PCRoom?
+
+        let ex = expectation(description: "created room with name  \(roomName)")
+
+        user.createRoom(name: roomName, addUserIds: addUserIds) { r, error in
+            XCTAssertNil(error)
+            XCTAssertNotNil(r)
+
+            room = r
+            ex.fulfill()
+        }
+
+        waitForExpectations(timeout: 5)
+        return room!
     }
 }
