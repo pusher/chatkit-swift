a53329ffc146f52b16c5abfaaf4b26f67fec3bd3
Callum Oakley <c.oakley108@gmail.com>
2018-07-30 15:29:09 +0100

    order fetch messages by increasing id

--- Sources/PCCurrentUser.swift
+++ Sources/PCCurrentUser.swift
@@ -1013,7 +1013,12 @@ public final class PCCurrentUser {
 
                             messages.append(message) {
                                 if progressCounter.incrementSuccessAndCheckIfFinished() {
-                                    completionHandler(messages.underlyingArray.sorted(by: { $0.id > $1.id }), nil)
+                                    completionHandler(
+                                        messages.underlyingArray.sorted(
+                                            by: { $0.id < $1.id }
+                                        ),
+                                        nil
+                                    )
                                 }
                             }
                         }
