c0bf966c1b02011beb5ccb29c8c9dccf6a2186cb
Hamilton Chapman <hamchapman@gmail.com>
2018-08-29 14:50:34 +0100

    Fix typo in example app

--- ChatkitiOSExample/ChatkitiOSExample/ViewController.swift
+++ ChatkitiOSExample/ChatkitiOSExample/ViewController.swift
@@ -70,7 +70,7 @@ class ViewController: UIViewController {
 //                    currentUser.sendMessage(
 //                        roomId: currentUser.rooms.last!.id,
 //                        text: "Just a message with an attachment",
-//                        attachmentType: .fileURL(imageURL, name: "cucas.jpg")
+//                        attachmentType: .fileURL(imageURL, name: "somdog.jpg")
 //    //                    attachmentType: .link("https://i.giphy.com/RpByGPT5VlZiE.gif", type: "image")
 //                    ) { messageId, err in
 //                        guard err == nil else {
