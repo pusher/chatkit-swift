edb6a07c097515483f1719205c662a5f89657f69
Hamilton Chapman <hamchapman@gmail.com>
2019-01-14 16:59:29 +0000

    Remove sleep from room tests

--- Tests/RoomTests.swift
+++ Tests/RoomTests.swift
@@ -19,26 +19,26 @@ class RoomTests: XCTestCase {
         deleteInstanceResources() { err in
             XCTAssertNil(err)
             deleteResourcesEx.fulfill()
-
-            createStandardInstanceRoles() { err in
-                XCTAssertNil(err)
-                createRolesEx.fulfill()
-            }
-
-            createUser(id: "alice") { err in
-                XCTAssertNil(err)
-                createAliceEx.fulfill()
-            }
-
-            createUser(id: "bob") { err in
-                XCTAssertNil(err)
-                createBobEx.fulfill()
-            }
-
-            sleep(1)
         }
 
-        waitForExpectations(timeout: 15)
+        wait(for: [deleteResourcesEx], timeout: 15)
+
+        createStandardInstanceRoles() { err in
+            XCTAssertNil(err)
+            createRolesEx.fulfill()
+        }
+
+        createUser(id: "alice") { err in
+            XCTAssertNil(err)
+            createAliceEx.fulfill()
+        }
+
+        createUser(id: "bob") { err in
+            XCTAssertNil(err)
+            createBobEx.fulfill()
+        }
+
+        wait(for: [createRolesEx, createAliceEx, createBobEx], timeout: 15)
     }
 
     override func tearDown() {
