ce0e4a7505940412bf95c43434c54b1e41c3b04b
Hamilton Chapman <hamchapman@gmail.com>
2019-01-31 14:48:58 +0000

    Add a test for connection still succeeding even if the current user got a read cursor for a deleted room

--- Tests/ConnectionTests.swift
+++ Tests/ConnectionTests.swift
@@ -73,4 +73,59 @@ class ConnectionTests: XCTestCase {
 
         wait(for: connectionExpectations, timeout: 30)
     }
+
+    func testConnectionWorksWhenCurrentUserReceivesAReadCursorForADeletedRoom() {
+        let userCreatedEx = expectation(description: "alice created")
+        let roomCreatedEx = expectation(description: "room created")
+        let roomDeletedEx = expectation(description: "room deleted")
+        let readCursorSetEx = expectation(description: "read cursor set")
+        let connectedEx = expectation(description: "alice connected")
+
+        let userID = "alice"
+
+        createUser(id: userID) { err in
+            XCTAssertNil(err)
+            userCreatedEx.fulfill()
+        }
+        wait(for: [userCreatedEx], timeout: 15)
+
+        var roomID: String!
+
+        createRoom(creatorID: userID, name: "test room") { err, data in
+            XCTAssertNil(err)
+            XCTAssertNotNil(data)
+            let roomObject = try! JSONSerialization.jsonObject(with: data!, options: []) as! [String: Any]
+            let roomIDFromJSON = roomObject["id"] as! String
+            roomID = roomIDFromJSON
+
+            roomCreatedEx.fulfill()
+        }
+        wait(for: [roomCreatedEx], timeout: 15)
+
+        deleteRoom(id: roomID) { err in
+            XCTAssertNil(err)
+            roomDeletedEx.fulfill()
+        }
+        wait(for: [roomDeletedEx], timeout: 15)
+
+        setReadCursor(
+            userID: userID,
+            roomID: roomID,
+            position: 100
+        ) { err in
+            XCTAssertNil(err)
+            readCursorSetEx.fulfill()
+        }
+        wait(for: [readCursorSetEx], timeout: 15)
+
+        let aliceChatManager = newTestChatManager(userID: userID)
+
+        aliceChatManager.connect(delegate: TestingChatManagerDelegate()) { alice, err in
+            XCTAssertNil(err)
+            XCTAssertEqual(alice!.cursorStore.cursors.keys.count, 0)
+            connectedEx.fulfill()
+        }
+
+        wait(for: [connectedEx], timeout: 15)
+    }
 }
