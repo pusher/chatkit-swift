a38e0e15a963a661ae4da53c73b8ba4b04677ba6
Hamilton Chapman <hamchapman@gmail.com>
2019-01-31 14:46:45 +0000

    Restructure room tests a little to hopefully make them more reliable

--- Tests/RoomTests.swift
+++ Tests/RoomTests.swift
@@ -84,57 +84,58 @@ class RoomTests: XCTestCase {
         assignGlobalRole("admin", toUser: "alice") { err in
             XCTAssertNil(err)
             assignAdminRoleEx.fulfill()
+        }
+        wait(for: [assignAdminRoleEx], timeout: 15)
 
-            self.aliceChatManager.connect(delegate: TestingChatManagerDelegate()) { alice, err in
+        self.aliceChatManager.connect(delegate: TestingChatManagerDelegate()) { alice, err in
+            XCTAssertNil(err)
+
+            alice!.createRoom(
+                name: "mushroom",
+                isPrivate: true,
+                addUserIDs: ["bob"],
+                customData: ["testing": "some custom data", "and more": 123]
+            ) { room, err in
                 XCTAssertNil(err)
+                XCTAssertNotNil(room)
+                XCTAssertEqual(room!.name, "mushroom")
+                XCTAssertTrue(room!.isPrivate)
+                let expectedUserIDs = ["alice", "bob"]
+                let sortedUserIDs = room!.users.map { $0.id }.sorted()
+                XCTAssertEqual(sortedUserIDs, expectedUserIDs)
+                XCTAssertEqual(room!.customData!.keys.count, 2)
+                XCTAssertEqual((room!.customData!["testing"] as! String), "some custom data")
+                XCTAssertEqual(room!.customData!["and more"] as! Int, 123)
+                roomCreatedEx.fulfill()
 
-                alice!.createRoom(
-                    name: "mushroom",
-                    isPrivate: true,
-                    addUserIDs: ["bob"],
-                    customData: ["testing": "some custom data", "and more": 123]
-                ) { room, err in
+                alice!.updateRoom(
+                    id: room!.id,
+                    name: "balloon",
+                    isPrivate: false,
+                    customData: ["different": "custom stuff", "and": ["nested": "stuff"]]
+                ) { err in
                     XCTAssertNil(err)
-                    XCTAssertNotNil(room)
-                    XCTAssertEqual(room!.name, "mushroom")
-                    XCTAssertTrue(room!.isPrivate)
-                    let expectedUserIDs = ["alice", "bob"]
-                    let sortedUserIDs = room!.users.map { $0.id }.sorted()
-                    XCTAssertEqual(sortedUserIDs, expectedUserIDs)
-                    XCTAssertEqual(room!.customData!.keys.count, 2)
-                    XCTAssertEqual((room!.customData!["testing"] as! String), "some custom data")
-                    XCTAssertEqual(room!.customData!["and more"] as! Int, 123)
-                    roomCreatedEx.fulfill()
+                    roomUpdatedEx.fulfill()
 
-                    alice!.updateRoom(
-                        id: room!.id,
-                        name: "balloon",
-                        isPrivate: false,
-                        customData: ["different": "custom stuff", "and": ["nested": "stuff"]]
-                    ) { err in
+                    alice!.roomStore.getRoom(id: room!.id) { updatedRoom, err in
                         XCTAssertNil(err)
-                        roomUpdatedEx.fulfill()
-
-                        alice!.roomStore.getRoom(id: room!.id) { updatedRoom, err in
-                            XCTAssertNil(err)
-                            XCTAssertNotNil(updatedRoom)
-                            XCTAssertEqual(updatedRoom!.name, "balloon")
-                            XCTAssertFalse(updatedRoom!.isPrivate)
-                            XCTAssertEqual(updatedRoom!.customData!.keys.count, 2)
-                            XCTAssertEqual((updatedRoom!.customData!["different"] as! String), "custom stuff")
-                            XCTAssertEqual(updatedRoom!.customData!["and"] as! [String: String], ["nested": "stuff"])
-                            roomFetchEx.fulfill()
-
-                            assignGlobalRole("default", toUser: "alice") { err in
-                                XCTAssertNil(err)
-                                unassignAdminRoleEx.fulfill()
-                            }
-                        }
+                        XCTAssertNotNil(updatedRoom)
+                        XCTAssertEqual(updatedRoom!.name, "balloon")
+                        XCTAssertFalse(updatedRoom!.isPrivate)
+                        XCTAssertEqual(updatedRoom!.customData!.keys.count, 2)
+                        XCTAssertEqual((updatedRoom!.customData!["different"] as! String), "custom stuff")
+                        XCTAssertEqual(updatedRoom!.customData!["and"] as! [String: String], ["nested": "stuff"])
+                        roomFetchEx.fulfill()
                     }
                 }
             }
         }
+        wait(for: [roomCreatedEx, roomUpdatedEx, roomFetchEx], timeout: 15)
 
-        waitForExpectations(timeout: 15)
+        assignGlobalRole("default", toUser: "alice") { err in
+            XCTAssertNil(err)
+            unassignAdminRoleEx.fulfill()
+        }
+        wait(for: [unassignAdminRoleEx], timeout: 15)
     }
 }
