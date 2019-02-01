16827dd0dd46a07041b71d7e03291a27e2da3b2a
Hamilton Chapman <hamchapman@gmail.com>
2019-02-01 11:32:22 +0000

    Add public init for PCAttachment. Fixes #134

--- Sources/PCAttachment.swift
+++ Sources/PCAttachment.swift
@@ -5,4 +5,10 @@ public struct PCAttachment {
     public let link: String
     public let type: String
     public let name: String
+
+    public init(link: String, type: String, name: String) {
+        self.link = link
+        self.type = type
+        self.name = name
+    }
 }
