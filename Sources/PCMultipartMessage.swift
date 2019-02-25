d51f5439cfbd2f738deaeb880c47974dfba115ef
Vivan Kumar <vivangkumar@gmail.com>
2019-02-25 14:40:34 +0000

    Implement attachment refresh functionality

--- Sources/PCMultipartMessage.swift
+++ Sources/PCMultipartMessage.swift
@@ -79,12 +79,95 @@ extension PCMultipartPayload: Equatable {
     }
 }
 
-public struct PCMultipartAttachmentPayload {
+public class PCMultipartAttachmentPayload {
     let id: String
-    let downloadUrl: String
-    let refreshUrl: String
-    let expiration: String
     let name: String?
     let customData: [String: Any]?
     let size: Int
+    private let urlRefresher: PCMultipartAttachmentUrlRefresher
+    internal let refreshUrl: String
+    internal var downloadUrl: String
+    internal var expiration: String
+
+    init(
+        id: String,
+        name: String?,
+        customData: [String: Any]?,
+        size: Int,
+        urlRefresher: PCMultipartAttachmentUrlRefresher,
+        refreshUrl: String,
+        downloadUrl: String,
+        expiration: String
+    ) {
+        self.id = id
+        self.name = name
+        self.customData = customData
+        self.size = size
+        self.urlRefresher = urlRefresher
+        self.refreshUrl = refreshUrl
+        self.downloadUrl = downloadUrl
+        self.expiration = expiration
+    }
+
+    func url() -> String {
+        if (Date() > self.urlExpiry()) {
+            self.urlRefresher.refresh(attachment: self) { newAttachment, error in
+                guard error == nil else {
+                    return
+                }
+
+                if newAttachment != nil {
+                    self.downloadUrl = newAttachment!.downloadUrl
+                    self.expiration = newAttachment!.expiration
+                }
+            }
+        }
+
+        return downloadUrl
+    }
+
+    func urlExpiry() -> Date {
+        return PCDateFormatter.shared.formatString(self.expiration)
+    }
+}
+
+public class PCMultipartAttachmentUrlRefresher {
+    let client: Instance
+
+    init(client: Instance) {
+        self.client = client
+    }
+
+    func refresh(
+        attachment: PCMultipartAttachmentPayload,
+        completionHandler: @escaping (PCMultipartAttachmentPayload?, Error?) -> Void
+    ) {
+        let request = PPRequestOptions(method: HTTPMethod.GET.rawValue, path: attachment.refreshUrl)
+        self.client.request(
+            using: request,
+            onSuccess: { data in
+                guard let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []) else {
+                    self.client.logger.log("Failed to serialse attachment refresh attachment response", logLevel: .error)
+                    return
+                }
+
+                guard let attachmentPayload = jsonObject as? [String: Any] else {
+                    self.client.logger.log("Failed to assert refresh attachment response as a map", logLevel: .error)
+                    return
+                }
+
+                guard let newAttachment = try? PCPayloadDeserializer.createMultipartAttachmentFromPayload(attachmentPayload, urlRefresher: self) else {
+                    self.client.logger.log("Failed to deserialise attachment payload into attachment class", logLevel: .error)
+                    return
+                }
+
+                completionHandler(newAttachment, nil)
+                return
+            },
+            onError: { error in
+                completionHandler(nil, error)
+                return
+            }
+        )
+    }
 }
