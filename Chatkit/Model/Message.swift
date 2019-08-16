05a380be02e6e6e08466166ed39fbe62b9c8a4de
Grzegorz Kozłowski <grzegorz.kozlowski@me.com>
2019-08-16 16:29:07 +0200

    Implement snapshotting mechanism for Message.

--- Chatkit/Model/Message.swift
+++ Chatkit/Model/Message.swift
@@ -1,6 +1,46 @@
 import Foundation
 
 public struct Message {
+    
+    public let identifier: String
+    public let sender: User
+    public let parts: Set<MessagePart>
+    public let createdAt: Date
+    public let updatedAt: Date
+    public let deletedAt: Date?
+    // TODO: Implement cursors
+    
+    // MARK: - Initializers
+    
+    init(identifier: String, sender: User, parts: Set<MessagePart>, createdAt: Date, updatedAt: Date, deletedAt: Date?) {
+        self.identifier = identifier
+        self.sender = sender
+        self.parts = parts
+        self.createdAt = createdAt
+        self.updatedAt = updatedAt
+        self.deletedAt = deletedAt
+    }
+    
+}
+
+// MARK: - Hashable
+
+extension Message: Hashable {
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(self.identifier)
+    }
+    
+}
+
+// MARK: - Equatable
+
+extension Message: Equatable {
+    
+    public static func == (lhs: Message, rhs: Message) -> Bool {
+        return lhs.identifier == rhs.identifier && lhs.sender == rhs.sender && lhs.parts == rhs.parts && lhs.createdAt == rhs.createdAt && lhs.updatedAt == rhs.updatedAt && lhs.deletedAt == rhs.deletedAt
+    }
+    
 }
 
 // MARK: - Model
--- Chatkit/Persistence/Snapshots/Entities/MessageEntity+Snapshotable.swift
+++ Chatkit/Persistence/Snapshots/Entities/MessageEntity+Snapshotable.swift
@@ -9,8 +9,35 @@ extension MessageEntity: Snapshotable {
     // MARK: - Internal methods
     
     func snapshot() throws -> Message {
-        // TODO: Implement
-        return Message()
+        let sender = try self.sender.snapshot()
+        let parts = try snapshotParts()
+        
+        return Message(identifier: self.identifier, sender: sender, parts: parts, createdAt: self.createdAt, updatedAt: self.updatedAt, deletedAt: self.deletedAt)
+    }
+    
+    // MARK: - Private methods
+    
+    private func snapshotParts() throws -> Set<MessagePart> {
+        let parts: [MessagePart] = self.parts.compactMap { part in
+            if let part = part as? InlinePartEntity {
+                return try? part.snapshot()
+            }
+            else if let part = part as? URLPartEntity {
+                return try? part.snapshot()
+            }
+            else if let part = part as? AttachmentPartEntity {
+                return try? part.snapshot()
+            }
+            else {
+                return nil
+            }
+        }
+        
+        guard parts.count > 0 else {
+            throw SnapshotError.snapshotFailure
+        }
+        
+        return Set(parts)
     }
     
 }
