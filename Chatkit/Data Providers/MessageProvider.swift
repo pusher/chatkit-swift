baa8db313b288fbe209b4014ea3321c1ebe20757
Grzegorz Kozłowski <grzegorz.kozlowski@me.com>
2019-08-21 10:32:26 +0100

    Add predicates to MessageProvider.

--- Chatkit/Data Providers/MessageProvider.swift
+++ Chatkit/Data Providers/MessageProvider.swift
@@ -1,7 +1,7 @@
 import Foundation
 import PusherPlatform
 
-public class MessageProvider {
+public struct MessageProvider {
     
     // MARK: - Properties
     
@@ -14,23 +14,23 @@ public class MessageProvider {
     }
     
     public var messagesWithText: [Message]? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%@ IN %K", InlinePartEntity.entity(), #keyPath(MessageEntity.parts.entity))
+        return self.store.objects(for: predicate)
     }
     
     public var messagesWithURL: [Message]? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%@ IN %K", URLPartEntity.entity(), #keyPath(MessageEntity.parts.entity))
+        return self.store.objects(for: predicate)
     }
     
     public var messagesWithAttachment: [Message]? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%@ IN %K", AttachmentPartEntity.entity(), #keyPath(MessageEntity.parts.entity))
+        return self.store.objects(for: predicate)
     }
     
     public var lastMessage: Message? {
-        // TODO: Implement
-        return nil
+        let sortDescriptor = NSSortDescriptor(key: #keyPath(MessageEntity.createdAt), ascending: false)
+        return self.store.object(orderedBy: [sortDescriptor])
     }
     
     // MARK: - Initializers
@@ -42,83 +42,86 @@ public class MessageProvider {
     // MARK: - Public methods
     
     public func message(with identifier: String) -> Message? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%K = %@", #keyPath(MessageEntity.identifier), identifier)
+        return self.store.object(for: predicate)
     }
     
     public func messages(of user: User) -> [Message]? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%K == %@", #keyPath(MessageEntity.sender), user.objectID)
+        return self.store.objects(for: predicate)
     }
     
     public func messages(in room: Room) -> [Message]? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%K == %@", #keyPath(MessageEntity.room), room.objectID)
+        return self.store.objects(for: predicate)
     }
     
     public func messages(of user: User, in room: Room) -> [Message]? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%K == %@ AND %K == %@", #keyPath(MessageEntity.sender), user.objectID, #keyPath(MessageEntity.room), room.objectID)
+        return self.store.objects(for: predicate)
     }
     
     public func messagesWithText(of user: User) -> [Message]? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%K == %@ AND %@ IN %K", #keyPath(MessageEntity.sender), user.objectID, InlinePartEntity.entity(), #keyPath(MessageEntity.parts.entity))
+        return self.store.objects(for: predicate)
     }
     
     public func messagesWithText(in room: Room) -> [Message]? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%K == %@ AND %@ IN %K", #keyPath(MessageEntity.room), room.objectID, InlinePartEntity.entity(), #keyPath(MessageEntity.parts.entity))
+        return self.store.objects(for: predicate)
     }
     
     public func messagesWithText(of user: User, in room: Room) -> [Message]? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%K == %@ AND %K == %@ AND %@ IN %K", #keyPath(MessageEntity.sender), user.objectID, #keyPath(MessageEntity.room), room.objectID, InlinePartEntity.entity(), #keyPath(MessageEntity.parts.entity))
+        return self.store.objects(for: predicate)
     }
     
     public func messagesWithURL(of user: User) -> [Message]? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%K == %@ AND %@ IN %K", #keyPath(MessageEntity.sender), user.objectID, URLPartEntity.entity(), #keyPath(MessageEntity.parts.entity))
+        return self.store.objects(for: predicate)
     }
     
     public func messagesWithURL(in room: Room) -> [Message]? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%K == %@ AND %@ IN %K", #keyPath(MessageEntity.room), room.objectID, URLPartEntity.entity(), #keyPath(MessageEntity.parts.entity))
+        return self.store.objects(for: predicate)
     }
     
     public func messagesWithURL(of user: User, in room: Room) -> [Message]? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%K == %@ AND %K == %@ AND %@ IN %K", #keyPath(MessageEntity.sender), user.objectID, #keyPath(MessageEntity.room), room.objectID, URLPartEntity.entity(), #keyPath(MessageEntity.parts.entity))
+        return self.store.objects(for: predicate)
     }
     
     public func messagesWithAttachment(of user: User) -> [Message]? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%K == %@ AND %@ IN %K", #keyPath(MessageEntity.sender), user.objectID, AttachmentPartEntity.entity(), #keyPath(MessageEntity.parts.entity))
+        return self.store.objects(for: predicate)
     }
     
     public func messagesWithAttachment(in room: Room) -> [Message]? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%K == %@ AND %@ IN %K", #keyPath(MessageEntity.room), room.objectID, AttachmentPartEntity.entity(), #keyPath(MessageEntity.parts.entity))
+        return self.store.objects(for: predicate)
     }
     
     public func messagesWithAttachment(of user: User, in room: Room) -> [Message]? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%K == %@ AND %K == %@ AND %@ IN %K", #keyPath(MessageEntity.sender), user.objectID, #keyPath(MessageEntity.room), room.objectID, AttachmentPartEntity.entity(), #keyPath(MessageEntity.parts.entity))
+        return self.store.objects(for: predicate)
     }
     
     public func lastMessage(of user: User) -> Message? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%K == %@", #keyPath(MessageEntity.sender), user.objectID)
+        let sortDescriptor = NSSortDescriptor(key: #keyPath(MessageEntity.createdAt), ascending: false)
+        return self.store.object(for: predicate, orderedBy: [sortDescriptor])
     }
     
     public func lastMessage(in room: Room) -> Message? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%K == %@", #keyPath(MessageEntity.room), room.objectID)
+        let sortDescriptor = NSSortDescriptor(key: #keyPath(MessageEntity.createdAt), ascending: false)
+        return self.store.object(for: predicate, orderedBy: [sortDescriptor])
     }
     
     public func lastMessage(of user: User, in room: Room) -> Message? {
-        // TODO: Implement
-        return nil
+        let predicate = NSPredicate(format: "%K == %@ AND %K == %@", #keyPath(MessageEntity.sender), user.objectID, #keyPath(MessageEntity.room), room.objectID)
+        let sortDescriptor = NSSortDescriptor(key: #keyPath(MessageEntity.createdAt), ascending: false)
+        return self.store.object(for: predicate, orderedBy: [sortDescriptor])
     }
     
 }
