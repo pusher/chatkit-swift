f6f2bf4f978b30fc127e2d746929b8832571dfbb
Grzegorz Kozłowski <grzegorz.kozlowski@me.com>
2019-09-17 16:13:30 +0100

    Observe changes to Core Data store in MessageProvider.

--- Chatkit/Data Providers/MessageProvider.swift
+++ Chatkit/Data Providers/MessageProvider.swift
@@ -7,38 +7,33 @@ public class MessageProvider: NSObject, DataProvider {
     // MARK: - Properties
     
     public private(set) var isFetchingOlderMessages: Bool
+    public let logger: PPLogger?
     
     public weak var delegate: MessageProviderDelegate?
     
-    private let roomIdentifier: String
-    
-    private var messages: [Message]
-    private var timer: Timer?
+    private let persistenceController: PersistenceController
+    private let roomIdentifier: NSManagedObjectID
+    private var messages: [MessageEntity]
     
     // MARK: - Accessors
     
-//    public var count: Int {
-//    public var numberOfMessages: Int {
-//    public var numberOfFetchedMessages: Int {
-//    public var numberOfReceivedMessages: Int {
     public var numberOfAvailableMessages: Int {
         return self.messages.count
     }
     
     // MARK: - Initializers
     
-//    public init(room: Room) {
-//        self.roomIdentifier = room.identifier
-    public init(roomIdentifier: String) {
-        self.roomIdentifier = roomIdentifier
+    init(room: Room, persistenceController: PersistenceController, logger: PPLogger? = nil) {
+        self.persistenceController = persistenceController
+        self.roomIdentifier = room.objectID
         self.isFetchingOlderMessages = false
-        
-        self.messages = Factory.createMessages(amount: 10)
-        self.timer = nil
+        self.logger = logger
+        self.messages = []
         
         super.init()
         
-        self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(createNewMessage(_:)), userInfo: nil, repeats: true)
+        self.registerForNotifications()
+        self.reloadData()
     }
     
     // MARK: - Public methods
@@ -49,7 +44,7 @@ public class MessageProvider: NSObject, DataProvider {
             return nil
         }
         
-        return self.messages[index]
+        return (try? self.messages[index].snapshot()) ?? nil
     }
     
     public func fetchOlderMessages(numberOfMessages: UInt, completionHandler: ((Error?) -> Void)? = nil) {
@@ -64,29 +59,83 @@ public class MessageProvider: NSObject, DataProvider {
         
         self.isFetchingOlderMessages = true
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
-            self.isFetchingOlderMessages = false
-            
-            let receivedMessages = Factory.createMessages(amount: numberOfMessages)
-            self.messages.insert(contentsOf: receivedMessages, at: 0)
-            
-            self.delegate?.messageProvider(self, didReceiveMessagesWithRange: 0..<Int(numberOfMessages))
-            
-            if let completionHandler = completionHandler {
-                completionHandler(nil)
-            }
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0) {
+//            self.isFetchingOlderMessages = false
+//
+//            let receivedMessages = Factory.createMessages(amount: numberOfMessages)
+//            self.messages.insert(contentsOf: receivedMessages, at: 0)
+//
+//            self.delegate?.messageProvider(self, didReceiveMessagesWithRange: 0..<Int(numberOfMessages))
+//
+//            if let completionHandler = completionHandler {
+//                completionHandler(nil)
+//            }
+//        }
+    }
+    
+    // MARK: - Private methods
+    
+    private func registerForNotifications() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(managedObjectContextObjectsDidChange(_:)),
+                                               name: .NSManagedObjectContextObjectsDidChange,
+                                               object: self.persistenceController.mainContext)
+    }
+    
+    private func reloadData() {
+        let fetchRequest = NSFetchRequest<MessageEntity>(entityName: String(describing: MessageEntity.self))
+        fetchRequest.predicate = NSPredicate(format: "%K == #@", #keyPath(MessageEntity.room), self.roomIdentifier)
+        fetchRequest.sortDescriptors = [NSSortDescriptor(key: #keyPath(MessageEntity.identifier), ascending: true)]
+        fetchRequest.fetchBatchSize = 30
+        
+        do {
+            self.messages = try self.persistenceController.mainContext.fetch(fetchRequest)
+        } catch {
+            self.logger?.log("Failed to reload messages with error: \(error.localizedDescription)", logLevel: .warning)
         }
     }
     
-    // MARK: - Timers
+    private func filterInsertions(_ objects: Set<NSManagedObject>) {
+        let messages = objects.compactMap { $0 as? MessageEntity }
+        
+        guard messages.count > 0 else {
+            return
+        }
+        
+        // TODO: Insertion position?
+        
+        // TODO: Notification
+    }
     
-    @objc private func createNewMessage(_ sender: Timer) {
-        let message = Factory.createMessage()
-        self.messages.append(message)
+    private func filterUpdates(_ objects: Set<NSManagedObject>) {
+        let messages = objects.compactMap { $0 as? MessageEntity }
         
-        let range = Range<Int>(uncheckedBounds: (lower: self.messages.count - 1, upper: self.messages.count))
+        for message in messages {
+            // TODO: Notification
+        }
+    }
+    
+    private func filterDeletions(_ objects: Set<NSManagedObject>) {
+    }
+    
+    // MARK: - Notifications
+    
+    @objc private func managedObjectContextObjectsDidChange(_ notification: Notification) {
+        guard let userInfo = notification.userInfo else {
+            return
+        }
         
-        self.delegate?.messageProvider(self, didReceiveMessagesWithRange: range)
+        if let insertedObjects = (userInfo[NSInsertedObjectsKey] as? Set<NSManagedObject>), insertedObjects.count > 0 {
+            self.filterInsertions(insertedObjects)
+        }
+        
+        if let updatedObjects = userInfo[NSUpdatedObjectsKey] as? Set<NSManagedObject>, updatedObjects.count > 0 {
+            self.filterUpdates(updatedObjects)
+        }
+        
+        if let deletedObjects = userInfo[NSDeletedObjectsKey] as? Set<NSManagedObject>, deletedObjects.count > 0 {
+            self.filterDeletions(deletedObjects)
+        }
     }
     
     // MARK: - Memory management
@@ -104,67 +153,5 @@ public protocol MessageProviderDelegate: class {
     func messageProvider(_ messageProvider: MessageProvider, didReceiveMessagesWithRange range: Range<Int>)
     func messageProvider(_ messageProvider: MessageProvider, didChangeMessageAtIndex index: Int, previousValue: Message)
     func messageProvider(_ messageProvider: MessageProvider, didDeleteMessageAtIndex index: Int, previousValue: Message)
-    
-}
-
-// MARK: - Factory
-
-private extension MessageProvider {
-    
-    class Factory {
-        
-        // MARK: - Properties
-        
-        private static let persistenceController: PersistenceController = {
-            let model = NSManagedObjectModel.mergedModel(from: [Bundle.current])!
-            
-            let storeDescription = NSPersistentStoreDescription(inMemoryPersistentStoreDescription: ())
-            storeDescription.shouldAddStoreAsynchronously = false
-            
-            return try! PersistenceController(model: model, storeDescriptions: [storeDescription])
-        }()
-        
-        // MARK: - Internal methods
-        
-        class func createUser() -> User {
-            let now = Date()
-            let number = Int.random(in: 1..<10000)
-            let userManagedObjectID = Factory.persistenceController.mainContext.create(UserEntity.self).objectID
-            
-            return User(identifier: "user\(number)",
-                        name: "Greg \(number)",
-                        avatar: nil,
-                        presenceState: .unknown,
-                        metadata: nil,
-                        createdAt: now,
-                        updatedAt: now,
-                        objectID: userManagedObjectID)
-        }
-        
-        class func createMessage() -> Message {
-            let now = Date()
-            let number = Int.random(in: 1..<10000)
-            let messageManagedObjectID = Factory.persistenceController.mainContext.create(MessageEntity.self).objectID
-            let part = MessagePart.text("text/plain", "Message number \(number)")
-            
-            return Message(identifier: "testIdentifier",
-                           sender: Factory.createUser(),
-                           parts: [part],
-                           readByUsers: nil,
-                           lastReadByUsers: nil,
-                           createdAt: now,
-                           updatedAt: now,
-                           deletedAt: nil,
-                           objectID: messageManagedObjectID
-            )
-        }
-        
-        class func createMessages(amount: UInt) -> [Message] {
-            return (0..<amount).map { _ in
-                Factory.createMessage()
-            }
-        }
-        
-    }
     
 }
