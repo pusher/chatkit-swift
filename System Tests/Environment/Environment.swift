b01aa863fda626ad4973badc49bd67659af8af4a
Grzegorz Kozłowski <grzegorz.kozlowski@me.com>
2020-01-24 15:36:45 +0000

    Improve environment setup.

--- System Tests/Environment/Environment.swift
+++ System Tests/Environment/Environment.swift
@@ -4,17 +4,34 @@ struct Environment {
     
     // MARK: - Properties
     
-    static let instanceLocator: String = Environment.variable(named: "INSTANCE_LOCATOR", ciValue: CI.instanceLocator)
+    static let instanceLocator: String = Environment.variable(named: "INSTANCE_LOCATOR")
+    
+}
+
+// MARK: - Value retrieval
+
+extension Environment {
     
     // MARK: - Private methods
     
-    private static func variable(named name: String, ciValue: String) -> String {
-        guard let value = ProcessInfo.processInfo.environment[name], value != ProcessInfo.unspecifiedValue else {
-            assert(!ciValue.hasPrefix("$(") && !ciValue.hasSuffix(")"), "Please specify the value of the variable in either the enviroment variables section of your current run scheme or inject the value using Swift Variable Injector from Bitrise.")
+    private static func variable(named name: String) -> String {
+        if let environmentValue = self.environmentVariable(named: name), environmentValue != ProcessInfo.unspecifiedValue {
+            return environmentValue
+        }
+        else if let ciValue = self.ciVariable(named: name), !ciValue.hasPrefix("$(") && !ciValue.hasSuffix(")") {
             return ciValue
         }
         
-        return value
+        preconditionFailure("Please specify the value of the variable in either the enviroment variables section of your current run scheme or inject the value using Swift Variable Injector from Bitrise.")
+    }
+    
+    private static func environmentVariable(named name: String) -> String? {
+        return ProcessInfo.processInfo.environment[name]
+    }
+    
+    private static func ciVariable(named name: String) -> String? {
+        let mirror = Mirror(reflecting: CI())
+        return mirror.children.first { $0.label == name.camelcased(separator: "_") }?.value as? String
     }
     
 }
