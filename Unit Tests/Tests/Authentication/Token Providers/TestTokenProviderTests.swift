09e1023522532b7922f73f14d674b3f94880c933
Oliver Pearmain <oliver.pearmain@pusher.com>
2020-01-31 10:42:15 +0000

    The PusherPlatform `Token.token` property  was renamed to `Token.value`.  Updating code here accordingly.

--- Unit Tests/Tests/Authentication/Token Providers/TestTokenProviderTests.swift
+++ Unit Tests/Tests/Authentication/Token Providers/TestTokenProviderTests.swift
@@ -93,7 +93,7 @@ class TestTokenProviderTests: XCTestCase {
             return
         }
         
-        XCTAssertEqual(token.token, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1Nzk2MDQxNDcsImlhdCI6MTU3OTUxNzc0NywiaW5zdGFuY2UiOiI5NzU1MTZmMS1mOWUzLTRlNTUtYTQ0ZC1lNDA3OTIzMmY5NDciLCJpc3MiOiJhcGlfa2V5cy80ZTQyOWNjNS0wM2YzLTQwNzctYmY4ZC04YTcxYWMwYWM2ODgiLCJzdWIiOiJib2IifQ.5uyq_dBsGfdyqnDVDhm7d0R9w6HGApllBLVhwYHCNBI")
+        XCTAssertEqual(token.value, "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE1Nzk2MDQxNDcsImlhdCI6MTU3OTUxNzc0NywiaW5zdGFuY2UiOiI5NzU1MTZmMS1mOWUzLTRlNTUtYTQ0ZC1lNDA3OTIzMmY5NDciLCJpc3MiOiJhcGlfa2V5cy80ZTQyOWNjNS0wM2YzLTQwNzctYmY4ZC04YTcxYWMwYWM2ODgiLCJzdWIiOiJib2IifQ.5uyq_dBsGfdyqnDVDhm7d0R9w6HGApllBLVhwYHCNBI")
         XCTAssertEqual(token.expiryDate.timeIntervalSinceNow, 86400, accuracy: 0.01)
     }
     
@@ -175,7 +175,7 @@ class TestTokenProviderTests: XCTestCase {
             return
         }
         
-        XCTAssertGreaterThan(token.token.count, 0)
+        XCTAssertGreaterThan(token.value.count, 0)
     }
     
     func testShouldSetDefaultContentTypeBodyItem() {
@@ -219,7 +219,7 @@ class TestTokenProviderTests: XCTestCase {
             return
         }
         
-        XCTAssertGreaterThan(token.token.count, 0)
+        XCTAssertGreaterThan(token.value.count, 0)
     }
     
 }
