05987c8d5f38305c592f9477538d7ef3888d9f03
Grzegorz Kozłowski <grzegorz.kozlowski@me.com>
2020-02-21 14:17:13 +0000

    Add unit tests for joined rooms filter.

---
+++ Unit Tests/Tests/Repositories/Filters/JoinedRoomsFilterTests.swift
@@ -0,0 +1,462 @@
+import XCTest
+@testable import PusherChatkit
+
+class JoinedRoomsFilterTests: XCTestCase {
+    
+    // MARK: - Tests
+    
+    func test_hasModifiedSubstateWithEqualStates_returnsFalse() {
+        
+        /******************/
+        /*---- GIVEN -----*/
+        /******************/
+        
+        let oldState = VersionedState(
+            chatState: ChatState(
+                currentUser: .empty,
+                joinedRooms: RoomListState(
+                    rooms: [
+                        "test-identifier" : RoomState(
+                            identifier: "test-identifier",
+                            name: "test-name",
+                            isPrivate: true,
+                            pushNotificationTitle: "test-push-notification-title",
+                            customData: [
+                                "test-key" : "test-value"
+                            ],
+                            lastMessageAt: .distantPast,
+                            readSummary: ReadSummaryState(
+                                unreadCount: 10
+                            ),
+                            createdAt: .distantPast,
+                            updatedAt: .distantFuture
+                        )
+                    ]
+                ),
+                users: .empty
+            ),
+            version: 1,
+            signature: .initialState
+        )
+        
+        let newState = oldState
+        
+        let sut = JoinedRoomsRepository.Filter()
+        
+        /******************/
+        /*----- WHEN -----*/
+        /******************/
+        
+        let result = sut.hasModifiedSubstate(oldState: oldState, newState: newState)
+        
+        /******************/
+        /*----- THEN -----*/
+        /******************/
+        
+        XCTAssertFalse(result)
+    }
+    
+    func test_hasModifiedSubstateWithModifiedSubstateThatIsNotMonitoredByTheFilter_returnsFalse() {
+        
+        /******************/
+        /*---- GIVEN -----*/
+        /******************/
+        
+        let oldState = VersionedState(
+            chatState: ChatState(
+                currentUser: .empty,
+                joinedRooms: RoomListState(
+                    rooms: [
+                        "test-identifier" : RoomState(
+                            identifier: "test-identifier",
+                            name: "test-name",
+                            isPrivate: true,
+                            pushNotificationTitle: "test-push-notification-title",
+                            customData: [
+                                "test-key" : "test-value"
+                            ],
+                            lastMessageAt: .distantPast,
+                            readSummary: ReadSummaryState(
+                                unreadCount: 10
+                            ),
+                            createdAt: .distantPast,
+                            updatedAt: .distantFuture
+                        )
+                    ]
+                ),
+                users: .empty
+            ),
+            version: 1,
+            signature: .initialState
+        )
+        
+        let newState = VersionedState(
+            chatState: ChatState(
+                currentUser: .empty,
+                joinedRooms: RoomListState(
+                    rooms: [
+                        "test-identifier" : RoomState(
+                            identifier: "test-identifier",
+                            name: "test-name",
+                            isPrivate: true,
+                            pushNotificationTitle: "test-push-notification-title",
+                            customData: [
+                                "test-key" : "test-value"
+                            ],
+                            lastMessageAt: .distantPast,
+                            readSummary: ReadSummaryState(
+                                unreadCount: 10
+                            ),
+                            createdAt: .distantPast,
+                            updatedAt: .distantFuture
+                        )
+                    ]
+                ),
+                users: UserListState(
+                    users: [
+                        "test-user" : .partial(identifier: "test-user")
+                    ]
+                )
+            ),
+            version: 2,
+            signature: .addedToRoom
+        )
+        
+        let sut = JoinedRoomsRepository.Filter()
+        
+        /******************/
+        /*----- WHEN -----*/
+        /******************/
+        
+        let result = sut.hasModifiedSubstate(oldState: oldState, newState: newState)
+        
+        /******************/
+        /*----- THEN -----*/
+        /******************/
+        
+        XCTAssertFalse(result)
+    }
+    
+    func test_hasModifiedSubstateWithModifiedSubstateThatIsMonitoredByTheFilter_returnsTrue() {
+        
+        /******************/
+        /*---- GIVEN -----*/
+        /******************/
+        
+        let oldState = VersionedState(
+            chatState: ChatState(
+                currentUser: .empty,
+                joinedRooms: RoomListState(
+                    rooms: [
+                        "test-identifier" : RoomState(
+                            identifier: "test-identifier",
+                            name: "test-name",
+                            isPrivate: true,
+                            pushNotificationTitle: "test-push-notification-title",
+                            customData: [
+                                "test-key" : "test-value"
+                            ],
+                            lastMessageAt: .distantPast,
+                            readSummary: ReadSummaryState(
+                                unreadCount: 10
+                            ),
+                            createdAt: .distantPast,
+                            updatedAt: .distantFuture
+                        )
+                    ]
+                ),
+                users: .empty
+            ),
+            version: 1,
+            signature: .initialState
+        )
+        
+        let newState = VersionedState(
+            chatState: ChatState(
+                currentUser: .empty,
+                joinedRooms: .empty,
+                users: .empty
+            ),
+            version: 2,
+            signature: .roomDeleted
+        )
+        
+        let sut = JoinedRoomsRepository.Filter()
+        
+        /******************/
+        /*----- WHEN -----*/
+        /******************/
+        
+        let result = sut.hasModifiedSubstate(oldState: oldState, newState: newState)
+        
+        /******************/
+        /*----- THEN -----*/
+        /******************/
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_hasCompleteSubstateWithCompleteState_returnsTrue() {
+        
+        /******************/
+        /*---- GIVEN -----*/
+        /******************/
+        
+        let state = VersionedState(
+            chatState: ChatState(
+                currentUser: .empty,
+                joinedRooms: RoomListState(
+                    rooms: [
+                        "test-identifier" : RoomState(
+                            identifier: "test-identifier",
+                            name: "test-name",
+                            isPrivate: true,
+                            pushNotificationTitle: "test-push-notification-title",
+                            customData: [
+                                "test-key" : "test-value"
+                            ],
+                            lastMessageAt: .distantPast,
+                            readSummary: ReadSummaryState(
+                                unreadCount: 10
+                            ),
+                            createdAt: .distantPast,
+                            updatedAt: .distantFuture
+                        )
+                    ]
+                ),
+                users: .empty
+            ),
+            version: 1,
+            signature: .initialState
+        )
+        
+        let sut = JoinedRoomsRepository.Filter()
+        
+        /******************/
+        /*----- WHEN -----*/
+        /******************/
+        
+        let result = sut.hasCompleteSubstate(state)
+        
+        /******************/
+        /*----- THEN -----*/
+        /******************/
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_hasCompleteSubstateWithIncompleteSubstateThatIsMonitoredByTheFilter_returnsTrue() {
+        
+        /******************/
+        /*---- GIVEN -----*/
+        /******************/
+        
+        let state = VersionedState(
+            chatState: ChatState(
+                currentUser: .empty,
+                joinedRooms: RoomListState(
+                    rooms: [
+                        "test-identifier" : RoomState(
+                            identifier: "test-identifier",
+                            name: "test-name",
+                            isPrivate: true,
+                            pushNotificationTitle: "test-push-notification-title",
+                            customData: [
+                                "test-key" : "test-value"
+                            ],
+                            lastMessageAt: .distantPast,
+                            readSummary: ReadSummaryState(
+                                unreadCount: 10
+                            ),
+                            createdAt: .distantPast,
+                            updatedAt: .distantFuture
+                        )
+                    ]
+                ),
+                users: UserListState(
+                    users: [
+                        "user-identifier" : .partial(identifier: "user-identifier")
+                    ]
+                )
+            ),
+            version: 1,
+            signature: .initialState
+        )
+        
+        let sut = JoinedRoomsRepository.Filter()
+        
+        /******************/
+        /*----- WHEN -----*/
+        /******************/
+        
+        let result = sut.hasCompleteSubstate(state)
+        
+        /******************/
+        /*----- THEN -----*/
+        /******************/
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_hasSupportedSignatureWithInitialStateSignature_returnsTrue() {
+        
+        /******************/
+        /*---- GIVEN -----*/
+        /******************/
+        
+        let signature:  VersionSignature = .initialState
+        
+        let sut = JoinedRoomsRepository.Filter()
+        
+        /******************/
+        /*----- WHEN -----*/
+        /******************/
+        
+        let result = sut.hasSupportedSignature(signature)
+        
+        /******************/
+        /*----- THEN -----*/
+        /******************/
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_hasSupportedSignatureWithAddedToRoomSignature_returnsTrue() {
+        
+        /******************/
+        /*---- GIVEN -----*/
+        /******************/
+        
+        let signature:  VersionSignature = .addedToRoom
+        
+        let sut = JoinedRoomsRepository.Filter()
+        
+        /******************/
+        /*----- WHEN -----*/
+        /******************/
+        
+        let result = sut.hasSupportedSignature(signature)
+        
+        /******************/
+        /*----- THEN -----*/
+        /******************/
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_hasSupportedSignatureWithRemovedFromRoomSignature_returnsTrue() {
+        
+        /******************/
+        /*---- GIVEN -----*/
+        /******************/
+        
+        let signature:  VersionSignature = .removedFromRoom
+        
+        let sut = JoinedRoomsRepository.Filter()
+        
+        /******************/
+        /*----- WHEN -----*/
+        /******************/
+        
+        let result = sut.hasSupportedSignature(signature)
+        
+        /******************/
+        /*----- THEN -----*/
+        /******************/
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_hasSupportedSignatureWithRoomUpdatedSignature_returnsTrue() {
+        
+        /******************/
+        /*---- GIVEN -----*/
+        /******************/
+        
+        let signature:  VersionSignature = .roomUpdated
+        
+        let sut = JoinedRoomsRepository.Filter()
+        
+        /******************/
+        /*----- WHEN -----*/
+        /******************/
+        
+        let result = sut.hasSupportedSignature(signature)
+        
+        /******************/
+        /*----- THEN -----*/
+        /******************/
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_hasSupportedSignatureWithRoomDeletedSignature_returnsTrue() {
+        
+        /******************/
+        /*---- GIVEN -----*/
+        /******************/
+        
+        let signature:  VersionSignature = .roomDeleted
+        
+        let sut = JoinedRoomsRepository.Filter()
+        
+        /******************/
+        /*----- WHEN -----*/
+        /******************/
+        
+        let result = sut.hasSupportedSignature(signature)
+        
+        /******************/
+        /*----- THEN -----*/
+        /******************/
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_hasSupportedSignatureWithReadStateUpdatedSignature_returnsTrue() {
+        
+        /******************/
+        /*---- GIVEN -----*/
+        /******************/
+        
+        let signature:  VersionSignature = .readStateUpdated
+        
+        let sut = JoinedRoomsRepository.Filter()
+        
+        /******************/
+        /*----- WHEN -----*/
+        /******************/
+        
+        let result = sut.hasSupportedSignature(signature)
+        
+        /******************/
+        /*----- THEN -----*/
+        /******************/
+        
+        XCTAssertTrue(result)
+    }
+    
+    func test_hasSupportedSignatureWithUnsupportewdSignature_returnsTrue() {
+        
+        /******************/
+        /*---- GIVEN -----*/
+        /******************/
+        
+        let signature:  VersionSignature = .unsigned
+        
+        let sut = JoinedRoomsRepository.Filter()
+        
+        /******************/
+        /*----- WHEN -----*/
+        /******************/
+        
+        let result = sut.hasSupportedSignature(signature)
+        
+        /******************/
+        /*----- THEN -----*/
+        /******************/
+        
+        XCTAssertFalse(result)
+    }
+    
+}
